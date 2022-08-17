4b13b3546de397b16e1fcc3b7dfd460a3661f8c2
Đức Phạm Xuân <phamxuanduc1201@gmail.com>
2022-08-17 17:03:36 +0700

    xuất hóa đơn PDF, update rating

--- src/app/main/pages/user-order/user-order-detail/user-order-detail.component.ts
+++ src/app/main/pages/user-order/user-order-detail/user-order-detail.component.ts
@@ -3,6 +3,9 @@ import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
 import { UserOrderDetailService } from './user-order-detail.service';
 
+import jspdf from 'jspdf';
+import html2canvas from 'html2canvas';
+
 @Component({
   selector: 'app-user-order-detail',
   templateUrl: './user-order-detail.component.html',
@@ -132,6 +135,7 @@ export class UserOrderDetailComponent implements OnInit {
 
   public selectedOrder;
   public selectedOrderDetail;
+  
   
   constructor(private router: Router, private _userOrderDetailService: UserOrderDetailService) 
   { 
@@ -321,20 +325,27 @@ export class UserOrderDetailComponent implements OnInit {
     }
   }
 
-  // convetToPDF() {
-  //   var data = document.getElementById('contentToConvert');
-  //   html2canvas(data).then(canvas => {
-  //       // Few necessary setting options
-  //       var imgWidth = 208;
-  //       var pageHeight = 295;
-  //       var imgHeight = canvas.height * imgWidth / canvas.width;
-  //       var heightLeft = imgHeight;
-  //       const contentDataURL = canvas.toDataURL('image/png')
-  //       let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF
-  //       var position = 0;
-  //       pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)
-  //       pdf.save('new-file.pdf'); // Generated PDF
-  //   });
-  // }
+  convetToPDF() {
+    var data = document.getElementById('contentToConvert');
+    html2canvas(data).then(canvas => {
+        // Few necessary setting options
+        var imgWidth = 208;
+        var pageHeight = 295;
+        var imgHeight = canvas.height * imgWidth / canvas.width;
+        var heightLeft = imgHeight;
+        const contentDataURL = canvas.toDataURL('image/png')
+        let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF
+        var position = 0;
+        pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)
+        pdf.save('Order_'+ this.selectedOrder.id +'_date_'+ this.selectedOrder.createdDate + '.pdf'); // Generated PDF
+    });
+  }
 
+
+  playAudio(){
+    let audio = new Audio();
+    audio.src = "../../../../../assets/audio/ZURQ2FE-notification.mp3";
+    audio.load();
+    audio.play();
+  }
 }
